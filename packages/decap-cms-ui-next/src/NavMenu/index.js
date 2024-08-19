b3bc0edb3a3405358a93c78f8ffda601d66af479
Daniel Mendes <contact@daniel-mendes.ch>
2024-08-19 23:06:07 +0200

    feat(ui): implement NavMenu components
    Adds MobileNavMenu, NavMenu, NavMenuGroup, NavMenuGroupLabel,
    NavMenuItem and stories

---
+++ packages/decap-cms-ui-next/src/NavMenu/index.js
@@ -0,0 +1,6 @@
+export { default as NavMenu } from './NavMenu';
+export { default as NavMenuGroup } from './NavMenuGroup';
+export { default as NavMenuGroupLabel } from './NavMenuGroupLabel';
+export { default as NavMenuItem } from './NavMenuItem';
+export { ExternalLinkIcon } from './NavMenuItem';
+export { default as NavMenuSeparator } from '../Menu/MenuSeparator';
