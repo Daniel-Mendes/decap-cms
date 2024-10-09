bdc0736181c218c4d89305ef7ce27606fac082e5
Daniel Mendes <contact@daniel-mendes.ch>
2024-10-09 16:32:38 +0200

    refactor: bump and cleanup stylelint, eslint and prettier configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('node:fs');
 
 const packages = fs
   .readdirSync(`${__dirname}/packages`, { withFileTypes: true })
@@ -6,99 +6,48 @@ const packages = fs
   .map(dirent => dirent.name);
 
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:cypress/recommended',
-    'prettier',
-    'plugin:import/recommended',
-  ],
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-    jest: true,
-    'cypress/globals': true,
-  },
-  globals: {
-    DECAP_CMS_VERSION: false,
-    DECAP_CMS_APP_VERSION: false,
-    DECAP_CMS_CORE_VERSION: false,
-    CMS_ENV: false,
-  },
-  rules: {
-    'no-console': [0],
-    'react/prop-types': [0],
-    'import/no-named-as-default': 0,
-    'import/order': [
-      'error',
-      {
-        'newlines-between': 'always',
-        groups: [['builtin', 'external'], ['internal', 'parent', 'sibling', 'index'], ['type']],
-      },
-    ],
-    'no-duplicate-imports': 'error',
-    '@emotion/no-vanilla': 'error',
-    '@emotion/pkg-renaming': 'error',
-    '@emotion/import-from-emotion': 'error',
-    '@emotion/styled-import': 'error',
-    'require-atomic-updates': [0],
-    'object-shorthand': ['error', 'always'],
-    'func-style': ['error', 'declaration'],
-    'prefer-const': [
-      'error',
-      {
-        destructuring: 'all',
-      },
-    ],
-    'unicorn/prefer-string-slice': 'error',
-    'react/no-unknown-property': ['error', { ignore: ['css', 'bold', 'italic', 'delete'] }],
-  },
-  plugins: ['@emotion', 'cypress', 'unicorn'],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-    },
-    'import/core-modules': [...packages, 'decap-cms-app/dist/esm'],
-  },
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      parser: '@typescript-eslint/parser',
-      extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:cypress/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:unicorn/recommended",
         'plugin:import/recommended',
-        'plugin:import/typescript',
-      ],
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-        ecmaFeatures: {
-          jsx: true,
+        "plugin:import/typescript",
+        'plugin:@typescript-eslint/recommended',
+        "plugin:cypress/recommended",
+        "prettier"
+    ],
+    parser: '@typescript-eslint/parser',
+    plugins: ["@emotion", 'cypress', 'unicorn', '@typescript-eslint'],
+    root: true,
+    settings: {
+        react: {
+            version: 'detect',
         },
-      },
-      rules: {
-        'no-duplicate-imports': [0], // handled by @typescript-eslint
-        '@typescript-eslint/ban-types': [0], // TODO enable in future
-        '@typescript-eslint/no-non-null-assertion': [0],
-        '@typescript-eslint/consistent-type-imports': 'error',
-        '@typescript-eslint/explicit-function-return-type': [0],
-        '@typescript-eslint/explicit-module-boundary-types': [0],
-        '@typescript-eslint/no-duplicate-imports': 'error',
-        '@typescript-eslint/no-use-before-define': [
-          'error',
-          { functions: false, classes: true, variables: true },
-        ],
-      },
+        "import/resolver": {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+        "import/core-modules": [...packages]
     },
-  ],
-};
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        jest: true,
+        "cypress/globals": true
+    },
+    globals: {
+        DECAP_CMS_VERSION: false,
+        DECAP_CMS_APP_VERSION: false,
+        DECAP_CMS_CORE_VERSION: false,
+        CMS_ENV: false,
+    },
+}
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+const config = {
+  arrowParens: "avoid",
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 100
+}
+
+export default config;
